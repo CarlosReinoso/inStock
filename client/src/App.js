50504daa179dd85ed87c3bbb2fd8432b6a918109
CarlosReinoso <bless@carlosreinoso.co.uk>
2021-06-01 21:54:44 +0100

    Front-End: Sass Variables, Mixins, Breakpoints, Colors and Font-face

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
@@ -17,6 +17,10 @@ function App() {
         >
           Learn React
         </a>
+        <button>Save</button>
+        <button className="button-secondary">Save</button>
+        <button className="button-delete">Save</button>
+        <button>Save</button>
       </header>
     </div>
   );
