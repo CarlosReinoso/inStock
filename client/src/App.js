fbf34224eb72c1ac327d1cd1ba68cea660a8dfc8
CarlosReinoso <bless@carlosreinoso.co.uk>
2021-06-03 21:43:15 +0100

    adding git to right filepath

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,14 @@
 import logo from "./logo.svg";
 import "./App.scss";
 import Header from "./components/header/header";
+import WarehouseDetails from "./components/warehouseDetails/WarehouseDetails";
 import { Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Route path="/:location" component={Header} />
-
-      <button>Save</button>
-      <button className="button-secondary">Save</button>
-      <button className="button-delete">Save</button>
-      <button>Save</button>
-      <p>This is remy test</p>
+      <Route path="/warehouse-details" component={WarehouseDetails} />
     </div>
   );
 }
