14b409223222e3ae4d140d5f8b3247af20547ad0
CarlosReinoso <bless@carlosreinoso.co.uk>
2021-06-05 14:06:40 +0100

    made component files

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,20 @@
 
 import WarehouseList from './components/warehouseList/WarehouseList';
 import "./App.scss";
-
-import Header from "./components/Header/Header";
+import DeleteModal from './components/deleteModal/DeleteModal'
+import Header from "./components/header/Header";
 import InventoryList from "./components/InventoryList/InventoryList";
 import { Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      {/* just so i can navigate, can delete */}
-      <Link to="/warehouses">warehouse</Link>
       <Route path="/warehouses">
         <WarehouseList />
       </Route>
       <Route path="/:location" component={Header} />
+      <Route path="/delete-modal" component={DeleteModal} />
       <Route path="/inventory" component={InventoryList} />
-      <button>Save</button>
-      <button className="button-secondary">Save</button>
-      <button className="button-delete">Save</button>
-      <button>Save</button>
-      <p>This is remy test</p>
     </div>
   );
 }
