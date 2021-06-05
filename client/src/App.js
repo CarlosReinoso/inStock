b5b79cbf469a6c92c4ad947326ea3e78c8e2300e
CarlosReinoso <bless@carlosreinoso.co.uk>
2021-06-05 23:08:01 +0100

    Front-End: Add New Inventory Item Component (Responsive)

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import Header from "./components/header/Header";
 import WarehouseDetails from "./components/warehouseDetails/WarehouseDetails";
 import { Route } from "react-router-dom";
 import InventoryList from "./components/InventoryList/InventoryList";
+import AddInventoryItem from './components/addInventoryItem/AddInventoryItem';
 
 
 function App() {
@@ -16,7 +17,7 @@ function App() {
         <WarehouseList />
       </Route>
       <Route path="/:location" component={Header} />
-
+      <Route path="/addinvent" component={AddInventoryItem} />
       <Route path="/warehouse-details" component={WarehouseDetails} />
       <Route path="/inventory" component={InventoryList} />
     
